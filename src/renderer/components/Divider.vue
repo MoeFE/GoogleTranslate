56e85e357f3cc65fff3b2fe64c79ee4f1d036c47
u3u <quq@quq.cat>
2017-08-25 17:28:48 +0800

    分割线组件

---
+++ src/renderer/components/Divider.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="divider">
+    <slot />
+  </div>
+</template>
+<style lang="stylus" scoped>
+.divider
+  position relative
+  text-align center
+  color #ccc
+  &::before
+    content ''
+    position absolute
+    left 0
+    top 50%
+    transform translateY(-50%)
+    width 100%
+    height 1px
+    background #eee
+  .icon
+    position relative
+    z-index 1
+    font-size 20px
+    background #fff
+    padding 0 5px
+</style>
