2158d89c75d56ef170c08084ce982cf2199ec049
u3u <quq@quq.cat>
2017-08-25 17:28:19 +0800

    图标组件

---
+++ src/renderer/components/Icon.vue
@@ -0,0 +1,11 @@
+<template>
+  <i :class="`icon icon-${icon}`"></i>
+</template>
+<script>
+export default {
+  name: 'icon',
+  props: {
+    icon: { type: String, required: true }
+  }
+}
+</script>
