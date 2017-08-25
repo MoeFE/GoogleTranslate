8d147de15aa07ceefc58bd4b48fd981472b0273d
u3u <quq@quq.cat>
2017-08-25 17:28:39 +0800

    头部组件

---
+++ src/renderer/components/Header.vue
@@ -0,0 +1,49 @@
+<template>
+  <header>
+    <slot name="fixed" />
+    <h1>Googl<i>e</i> {{ title }}</h1>
+    <slot name="settings" />
+  </header>
+</template>
+<script>
+export default {
+  name: 'header',
+  props: {
+    title: { type: String, default: 'Translate', required: true }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+header
+  display flex
+  align-items center
+  color #fff
+  background #4286f5
+  padding 10px
+  border-radius 6px 6px 0 0
+  cursor default
+  .icon
+    color #eee
+    transition color .3s
+    &:hover
+      color #fff
+  .icon-fixed
+    transition transform .3s
+    transform rotate(-45deg)
+  .icon-settings::after
+    content '\e601'
+    font-family icon
+  > *
+    font-size 18px
+  h1
+    margin 0
+    font-family google
+    font-weight: 500
+    text-align center
+    flex 1
+    i
+      display inline-block
+      font-style normal
+      transform rotate(-10deg)
+</style>
