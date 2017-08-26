539c4df393563a4ecb550275be10425ed6ea01d6
u3u <quq@quq.cat>
2017-08-26 16:47:49 +0800

    图标组件添加旋转属性

--- src/renderer/components/Icon.vue
+++ src/renderer/components/Icon.vue
@@ -1,11 +1,12 @@
 <template>
-  <i :class="`icon icon-${icon}`"></i>
+  <i :class="`icon icon-${icon}`" :style="{ transform: `rotate(${rotate}deg)` }"></i>
 </template>
 <script>
 export default {
   name: 'icon',
   props: {
-    icon: { type: String, required: true }
+    icon: { type: String, required: true },
+    rotate: { type: Number, required: false }
   }
 }
 </script>
