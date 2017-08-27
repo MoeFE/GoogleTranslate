9579ced75e9a6b5e89d2c3b75ee4d07bc2cb059f
u3u <quq@quq.cat>
2017-08-27 16:20:18 +0800

    国家图标组件

---
+++ src/renderer/components/Country.vue
@@ -0,0 +1,13 @@
+<template>
+  <img :src="`http://${ssl ? 'ssl-' : ''}api.itranslateapp.com/flags/${code}${retina ? '-2x' : ''}.png`">
+</template>
+<script>
+export default {
+  name: 'country',
+  props: {
+    ssl: { type: Boolean, default: false, required: false },
+    code: { type: String, default: 'auto', required: false },
+    retina: { type: Boolean, default: true, required: false }
+  }
+}
+</script>
