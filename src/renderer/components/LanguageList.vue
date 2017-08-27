66a105ee9406b5e7c57d467199feb02a18e00677
u3u <quq@quq.cat>
2017-08-27 16:23:52 +0800

    语言列表

---
+++ src/renderer/components/LanguageList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="language-list">
+    <h4>{{ title }}</h4>
+    <ul>
+      <slot />
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'language-list',
+  props: {
+    title: { type: String, default: '标题', required: true }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+h4
+  color #b1b9c0
+  margin 0 8px
+  padding 0 15px
+  font-size 16.5px
+ul
+  margin 5px 0
+  padding 0
+  list-style none
+  font-weight 500
+</style>
