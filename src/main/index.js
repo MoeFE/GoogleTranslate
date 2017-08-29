a236adc35f06e41fc08c4818f55eeabfaaf76eea
u3u <quq@quq.cat>
2017-08-29 14:11:48 +0800

    修改标题

--- src/main/index.js
+++ src/main/index.js
@@ -22,6 +22,7 @@ function createWindow () {
    * Initial window options
    */
   mainWindow = new MenubarWindow({
+    title: 'Google Translate',
     icon: path.join(__static, 'icon.ico'),
     index: winURL,
     height: 190,
