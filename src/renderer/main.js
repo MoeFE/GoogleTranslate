db2e1d44bad0330def8ba7b8fbba936d0224f847
u3u <quq@quq.cat>
2017-08-30 15:30:03 +0800

    添加全局快捷键插件

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -5,6 +5,9 @@ import App from './App'
 import router from './router'
 import store from './store'
 
+import VueHotkey from 'v-hotkey'
+Vue.use(VueHotkey)
+
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
