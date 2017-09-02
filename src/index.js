28b7e85bceec5fde2c53b9a2e4b34b2e72a6abc4
u3u <quq@quq.cat>
2017-09-02 15:51:06 +0800

    优化下载脚本

--- src/index.js
+++ src/index.js
@@ -2,17 +2,24 @@ var request = require('request')
 var fs = require('fs')
 var path = require('path').resolve(__dirname, './renderer/assets/img')
 var langs = require('./renderer/assets/json/languages')
-var api = 'https://ssl-api.itranslateapp.com/flags/{lang}-2x.png'
-
-console.log(langs)
+var api = 'http://api.itranslateapp.com/flags/{lang}-2x.png'
 
 fs.mkdir(path, err => {
-  if (err) console.log(err)
-  Object.keys(langs).forEach(lang => {
+  if (err) {
+    console.log(err)
+    process.abort()
+    return
+  }
+  var count = 0
+  Object.keys(langs).forEach((lang, index, arr) => {
     var filename = `${path}/${lang}-2x.png`
     var stream = fs.createWriteStream(filename)
     request(api.replace('{lang}', lang)).pipe(stream).on('close', () => {
       console.log(`${filename} 下载完成`)
+      if (++count >= arr.length) {
+        console.log('所有任务下载完成')
+        process.abort()
+      }
     })
   })
 })
