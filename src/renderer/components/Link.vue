8682b74b1b77c044730a7e536401a12d79e92f87
u3u <quq@quq.cat>
2017-09-02 20:36:12 +0800

    超链接组件
    允许使用外部浏览器打开链接

---
+++ src/renderer/components/Link.vue
@@ -0,0 +1,26 @@
+<template>
+  <a :href="href" @click.prevent="openExternal">
+    <slot />
+  </a>
+</template>
+<script>
+const shell = require('electron').shell
+export default {
+  name: 'link',
+  props: {
+    href: { type: String, default: 'javascript:;', required: false },
+    external: { type: Boolean, default: true, required: false }
+  },
+  methods: {
+    openExternal () {
+      if (this.external) shell.openExternal(this.href)
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+a
+  color #008cff
+  text-decoration none
+  font-weight 500
+</style>
