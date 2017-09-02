20b8b8f78e00c5dc352e1df26a5c3ecdb1cdc42c
u3u <quq@quq.cat>
2017-09-03 02:47:53 +0800

    偏好设置页面

--- src/renderer/views/SettingsPage.vue
+++ src/renderer/views/SettingsPage.vue
@@ -4,19 +4,30 @@
       <main>
         <div class="pannel">
           <h3 class="title">自动开始</h3>
-          <div class="body">
-            <p>启动 Mac 时自动启动 Google Translate</p>
-          </div>
+          <div class="body">启动 Mac 时自动启动 Google Translate。</div>
+          <vSwitch v-model="autoStart" />
         </div>
         <div class="pannel">
           <h3 class="title">定义快捷键</h3>
           <div class="body">
-            <p>Google Translate 触手可及。使用此快捷键打开：</p>
+            <p>触手可及，使用此快捷键打开：</p>
+            <vInput class="hotkey" :readonly="true" placeholder="按下按键记录快捷键" value="" />
+          </div>
+        </div>
+        <div class="pannel">
+          <h3 class="title">翻译引擎</h3>
+          <div class="body">
+            <p>设置 Google Translate 默认使用的翻译引擎：</p>
+            <Radio name="engine" value="BaiDu" v-model="engine">百度</Radio>
+            <Radio name="engine" value="YouDao" v-model="engine">有道</Radio>
+            <Radio name="engine" value="Google" v-model="engine">谷歌</Radio>
+            <Radio name="engine" value="GoogleCN" v-model="engine">谷歌（国内）</Radio>
           </div>
         </div>
       </main>
       <footer>
-        <p><a class="link" href="https://github.com/MoeFE/GoogleTranslate">Google Translate</a></p>
+        <p class="help">给个 Star 鼓励一下吧 (ฅ´ω`ฅ)</p>
+        <p><vLink href="https://github.com/MoeFE/GoogleTranslate">Google Translate</vLink></p>
         <p class="help">1.0.0</p>
       </footer>
     </div>
@@ -25,12 +36,18 @@
 <script>
 import anime from 'animejs'
 import Layout from '@/views/_Layout'
+import Radio from '@/components/Radio'
+import vSwitch from '@/components/Switch'
+import vInput from '@/components/Input'
+import vLink from '@/components/Link'
 import { WindowHelper } from '../utils'
 export default {
   name: 'settings-page',
-  components: { Layout },
+  components: { Layout, Radio, vSwitch, vInput, vLink },
   data () {
     return {
+      autoStart: true,
+      engine: 'Google',
       view: {
         height: 660,
         animeOptions: { duration: 150, easing: 'easeOutQuart' },
@@ -53,7 +70,8 @@ export default {
 </script>
 <style lang="stylus" scoped>
 .pannel
-  padding 20px
+  position relative
+  padding 15px 20px
   margin-bottom 20px
   border-radius 6px
   background #ededed
@@ -68,8 +86,10 @@ export default {
     color #666
     font-size 14px
     margin 0
-    p
-      margin 0
+  .switch
+    position absolute
+    top 10px
+    right 20px
 .container
   display flex
   flex-direction column
@@ -77,6 +97,8 @@ export default {
   padding 20px
   box-sizing border-box
   font-size 14px
+  position relative
+  z-index 1
   main
     flex 1
     margin-bottom 20px
@@ -86,11 +108,14 @@ export default {
       margin 5px 0
       &:nth-last-of-type(1)
         margin 0
-  .link
-    color #008cff
-    text-decoration none
-    font-weight 500
   .help
     color #9ca3a9
     font-size 12px
+  input.hotkey
+    border 1px solid transparent
+    border-radius 4px
+    font-size 12px
+    padding 15px 10px
+    &:focus
+      border 1px solid #199bff
 </style>
