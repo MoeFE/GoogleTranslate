d5c029cab389e171e9add2044d83acd43e4745ac
u3u <quq@quq.cat>
2017-09-02 15:52:02 +0800

    使用本地图标模式

--- src/renderer/components/Country.vue
+++ src/renderer/components/Country.vue
@@ -1,5 +1,5 @@
 <template>
-  <img :src="`http://${ssl ? 'ssl-' : ''}api.itranslateapp.com/flags/${code}${retina ? '-2x' : ''}.png`" draggable="false">
+  <img :src="src" draggable="false">
 </template>
 <script>
 export default {
@@ -7,7 +7,14 @@ export default {
   props: {
     ssl: { type: Boolean, default: false, required: false },
     code: { type: String, default: 'auto', required: false },
-    retina: { type: Boolean, default: true, required: false }
+    retina: { type: Boolean, default: true, required: false },
+    local: { type: Boolean, default: true, required: false }
+  },
+  computed: {
+    src () {
+      if (this.local) return require(`../assets/img/${this.code}-2x.png`)
+      else return `http://${this.ssl ? 'ssl-' : ''}api.itranslateapp.com/flags/${this.code}${this.retina ? '-2x' : ''}.png`
+    }
   }
 }
 </script>
