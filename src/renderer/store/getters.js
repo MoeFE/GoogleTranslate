54e7042322ad2a3a252428a1867b77aa27db4e9a
u3u <quq@quq.cat>
2017-09-04 12:03:25 +0800

    修复 state 未更新 bug

--- src/renderer/store/getters.js
+++ src/renderer/store/getters.js
@@ -1,4 +1,4 @@
 import { GOOGLE_TRANSLATE_VUEX_STATE } from './types'
 
-const state = state => JSON.parse(localStorage.getItem(GOOGLE_TRANSLATE_VUEX_STATE)) || state
+const state = state => ({ ...state, ...JSON.parse(localStorage.getItem(GOOGLE_TRANSLATE_VUEX_STATE)) })
 export default { state }
--- src/renderer/store/mutations.js
+++ src/renderer/store/mutations.js
@@ -1,7 +1,10 @@
 import { GOOGLE_TRANSLATE_VUEX_STATE, SAVE_STATE } from './types'
 
 export const mutations = {
-  [SAVE_STATE]: (state, payload) => localStorage.setItem(GOOGLE_TRANSLATE_VUEX_STATE, JSON.stringify(payload))
+  [SAVE_STATE]: (state, payload) => {
+    Object.keys(state).forEach(key => (state[key] = payload[key]))
+    localStorage.setItem(GOOGLE_TRANSLATE_VUEX_STATE, JSON.stringify(payload))
+  }
 }
 
 export default mutations
