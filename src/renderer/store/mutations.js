f8a194b77fdecacf80f41def018d1ca237cc004d
u3u <quq@quq.cat>
2017-09-04 11:14:24 +0800

    不要在此处合并

--- src/renderer/store/mutations.js
+++ src/renderer/store/mutations.js
@@ -1,7 +1,7 @@
 import { GOOGLE_TRANSLATE_VUEX_STATE, SAVE_STATE } from './types'
 
 export const mutations = {
-  [SAVE_STATE]: (state, payload) => localStorage.setItem(GOOGLE_TRANSLATE_VUEX_STATE, JSON.stringify({ ...state, ...payload }))
+  [SAVE_STATE]: (state, payload) => localStorage.setItem(GOOGLE_TRANSLATE_VUEX_STATE, JSON.stringify(payload))
 }
 
 export default mutations
