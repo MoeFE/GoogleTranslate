0882ccd5bb89546c86c6780d601a56ef2ed745bc
u3u <quq@quq.cat>
2017-09-04 21:55:05 +0800

    删除 /landing 路由
    坑爹的全局 css 污染我的样式！

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -6,11 +6,6 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
-      path: '/landing',
-      name: 'landing-page',
-      component: require('@/views/LandingPage')
-    },
-    {
       path: '/',
       name: 'transition-page',
       component: require('@/views/TransitionPage')
