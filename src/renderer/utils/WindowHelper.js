f16c65376244fd22c5b8dca98e39660a3c291e35
u3u <quq@quq.cat>
2017-09-19 15:10:37 +0800

    防止抖动

--- src/renderer/utils/WindowHelper.js
+++ src/renderer/utils/WindowHelper.js
@@ -1,9 +1,11 @@
 import anime from 'animejs'
 export class WindowHelper {
+  static anime = null
   static setSize (width, height, animate) {
     const targets = { width: window.innerWidth, height: window.innerHeight }
+    if (WindowHelper.anime) WindowHelper.anime.pause()
     if (width === targets.width && height === targets.height) window.resizeTo(targets.width, targets.height) // 防抖动
-    else anime({ targets, width, height, ...animate, update: () => window.resizeTo(targets.width, targets.height) })
+    else WindowHelper.anime = anime({ targets, width, height, ...animate, update: () => window.resizeTo(targets.width, targets.height) })
   }
 }
 export default WindowHelper
