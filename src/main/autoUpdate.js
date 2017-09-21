3128789ac65b6de3fd5b019fea967ba1bfeeb403
u3u <quq@quq.cat>
2017-09-21 10:56:40 +0800

    自动更新

--- src/main/autoUpdate.js
+++ src/main/autoUpdate.js
@@ -1,30 +1,59 @@
-import log from 'electron-log'
-import updater from 'electron-simple-updater'
-import pkg from '../../package.json'
+import fs from 'fs'
+import os from 'os'
+import path from 'path'
+import uuid from 'uuid'
+import events from 'events'
+import request from 'request'
+import progress from 'request-progress'
+import process from 'child_process'
+import { app } from 'electron'
+import { version, updater, build } from '../../package.json'
+const tmpdir = os.tmpdir()
+const guid = uuid()
 
-function checkForUpdates (win) {
-  updater.init({
-    url: pkg.updater.url,
-    version: pkg.version,
-    checkUpdateOnStart: false,
-    autoDownload: false,
-    logger: log
+function checkForUpdates (win, winURL) {
+  if (!win) return
+  if (!updater.url) return
+  win.updater = new events.EventEmitter()
+  const opt = {
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Electron/1.7.5 Safari/537.36'
+    }
+  }
+  request(updater.url, opt, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const meta = JSON.parse(body)
+      const zip = meta.assets.find(x => x.name === `${build.productName}-${meta.name}-mac.zip`)
+      if (!zip) return
+      if (version > meta.name) return
+
+      const savedir = path.resolve(tmpdir, `update-${guid}.zip`)
+      win.updater.meta = meta
+      win.updater.version = version
+      win.updater.downloadUpdate = () => {
+        progress(request(zip.browser_download_url))
+          .on('progress', state => win.updater.emit('update-progress', state))
+          .on('error', err => win.updater.emit('update-error', err))
+          .pipe(fs.createWriteStream(savedir))
+          .on('close', () => win.updater.emit('update-downloaded'))
+      }
+
+      win.updater.quitAndInstall = quit => {
+        process.exec(`unzip -o ${savedir} -d /Applications`, (error, stdout) => {
+          if (error) {
+            console.log(error)
+            return
+          }
+          const appName = stdout.match(/(\w+\.app)/)[0]
+          process.exec(`open /Applications/${appName}`)
+          fs.unlinkSync(savedir)
+          quit && app.quit()
+        })
+      }
+      win.loadURL(winURL + '#/update')
+      win.show()
+    } else console.log(response.statusCode, error)
   })
-
-  updater.on('update-available', (meta) => {
-    log.info('[updater] update avaiable: ', meta.version)
-    win.updater = updater
-    win.updater.meta = meta
-    win.show()
-  })
-
-  updater.on('update-not-available', () => log.warn('[updater] update not available.'))
-
-  updater.on('update-downloaded', (meta) => log.info('[updater] update downloaded: ', meta.version))
-
-  updater.on('error', (err) => log.error('[updater] update error: ', err))
-
-  updater.checkForUpdates()
 }
 
 export { checkForUpdates }
