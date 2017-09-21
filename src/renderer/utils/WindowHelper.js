3b56c56bdbb6e8287f396175389031cf2eef69f6
u3u <quq@quq.cat>
2017-09-21 13:57:03 +0800

    使用 lodash.throttle 防止抖动

--- src/renderer/utils/WindowHelper.js
+++ src/renderer/utils/WindowHelper.js
@@ -1,11 +1,17 @@
 import anime from 'animejs'
+import throttle from 'lodash.throttle'
+
 export class WindowHelper {
-  static anime = null
+  static _throttleSetSizeFunc = null
   static setSize (width, height, animate) {
-    const targets = { width: window.innerWidth, height: window.innerHeight }
-    if (WindowHelper.anime) WindowHelper.anime.pause()
-    if (width === targets.width && height === targets.height) window.resizeTo(targets.width, targets.height) // 防抖动
-    else WindowHelper.anime = anime({ targets, width, height, ...animate, update: () => window.resizeTo(targets.width, targets.height) })
+    if (WindowHelper._throttleSetSizeFunc === null) {
+      WindowHelper._throttleSetSizeFunc = throttle((width, height, animate) => {
+        const targets = { width: window.innerWidth, height: window.innerHeight }
+        if (width === targets.width && height === targets.height) window.resizeTo(targets.width, targets.height) // 防抖动
+        else anime({ targets, width, height, ...animate, update: () => window.resizeTo(targets.width, targets.height) })
+      }, animate.duration * 2 || 1000)
+    }
+    WindowHelper._throttleSetSizeFunc(width, height, animate)
   }
 }
 export default WindowHelper
