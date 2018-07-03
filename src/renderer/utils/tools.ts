62648e7f80f653c3b9c5b3ba4055c246224b5194
u3u <qwq@qwq.cat>
2018-07-03 14:40:51 +0800

    perf(utils): better window animation

--- src/renderer/utils/tools.ts
+++ src/renderer/utils/tools.ts
@@ -1,21 +1,25 @@
-import anime from 'animejs';
-import { Debounce } from 'lodash-decorators';
+import anime, { AnimeInstance } from 'animejs';
+
+let animeInstance: AnimeInstance;
 
 export default abstract class Tools {
   public static sleep(delay = 0) {
     return new Promise(resolve => setTimeout(resolve, delay));
   }
 
-  @Debounce(150, { leading: true, trailing: false })
   public static resize(width: number, height: number) {
-    const targets = { width: window.innerWidth, height: window.innerHeight };
-    anime({
-      targets,
-      width,
-      height,
-      duration: 150,
-      easing: 'easeOutQuart',
-      update: () => window.resizeTo(targets.width, targets.height),
+    return new Promise((resolve) => {
+      if (animeInstance) animeInstance.pause();
+      const targets = { width: window.innerWidth, height: window.innerHeight };
+      animeInstance = anime({
+        targets,
+        width,
+        height,
+        duration: 100,
+        easing: 'easeOutQuart',
+        update: () => window.resizeTo(targets.width, targets.height),
+        complete: resolve,
+      });
     });
   }
 }
