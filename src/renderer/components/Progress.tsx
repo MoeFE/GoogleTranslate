f994ec74ed877868735344a52c5c005b44c48e1a
u3u <qwq@qwq.cat>
2018-07-05 01:56:03 +0800

    feat(components): add progress comopnent

---
+++ src/renderer/components/Progress.tsx
@@ -0,0 +1,103 @@
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import { Prop } from 'vue-property-decorator';
+import styled from 'vue-emotion';
+import anime from 'animejs';
+
+const ProgressBar = styled.div`
+  position: relative;
+  display: inline-block;
+  width: 28px;
+  height: 28px;
+`;
+
+const Pause = styled.i`
+  display: flex;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 0;
+  &:before,
+  &:after {
+    content: '';
+    display: inline-block;
+    width: 3px;
+    height: 12px;
+    margin: 0 2px;
+    background: #3e83f8;
+  }
+`;
+
+export const Spin = styled.div`
+  position: relative;
+  width: 28px;
+  height: 28px;
+  border-radius: 28px;
+  border: 2px solid transparent;
+  border-top-color: #3e83f8;
+  border-right-color: #3e83f8;
+  border-bottom-color: rgba(62, 131, 248, 0);
+  border-left-color: rgba(62, 131, 248, 0);
+  box-sizing: border-box;
+  background: #fff;
+  background-clip: padding-box;
+  animation: loading 0.8s linear infinite;
+  @keyframes loading {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+@Component
+export default class Progress extends Vue {
+  @Prop({ type: Number, required: false, default: 0 })
+  private readonly value!: number;
+
+  @Prop({ type: Number, required: false, default: 7 })
+  private readonly strokeWidth!: number;
+
+  @Prop({ type: Number, required: false, default: 1000 })
+  private readonly duration!: number;
+
+  @Prop({ type: String, required: false, default: 'linear' })
+  private readonly easing!: string;
+
+  private path: string = 'M 50,50 m 0,-47 a 47,47 0 1 1 0,94 a 47,47 0 1 1 0,-94'; // prettier-ignore
+
+  private get strokeDashoffset() {
+    return (1 - this.value) * 295.416;
+  }
+
+  render() {
+    const { path, strokeWidth, strokeDashoffset } = this;
+
+    return (
+      <ProgressBar>
+        <svg viewBox="0 0 100 100">
+          <path
+            d={path}
+            stroke="transparent"
+            stroke-width={strokeWidth}
+            fill-opacity="0"
+          />
+          <path
+            d={path}
+            stroke="#3e83f8"
+            stroke-width={strokeWidth}
+            fill-opacity="0"
+            style={{
+              strokeDasharray: '295.416, 295.416',
+              strokeDashoffset,
+            }}
+          />
+        </svg>
+        <Pause />
+      </ProgressBar>
+    );
+  }
+}
