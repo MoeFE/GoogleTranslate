8e453b1e29123d64faa75f82c864aed79ef28127
u3u <qwq@qwq.cat>
2018-07-12 10:32:45 +0800

    chore(types): simplified definition

--- types/v-hotkey.d.ts
+++ types/v-hotkey.d.ts
@@ -1,7 +1,5 @@
 declare module 'v-hotkey' {
   import _Vue from 'vue';
 
-  export default abstract class VueHotkey {
-    public static install(Vue: typeof _Vue): void;
-  }
+  export default function install(Vue: typeof _Vue): void;
 }
