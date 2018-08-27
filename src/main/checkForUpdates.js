2be7e58ab2a98bd760d07c68e5519729524e714c
u3u <qwq@qwq.cat>
2018-08-27 16:27:21 +0800

    style: tweaks

--- src/main/checkForUpdates.js
+++ src/main/checkForUpdates.js
@@ -3,20 +3,15 @@ import download from 'download';
 import pkg from '../../package.json';
 
 export default async function checkForUpdates() {
-  const release = await download(
-    'https://api.github.com/repos/MoeFE/GoogleTranslate/releases/latest',
-  ).then(res => JSON.parse(res.toString()));
+  const url = 'https://api.github.com/repos/MoeFE/GoogleTranslate/releases/latest'; // prettier-ignore
+  const release = await download(url).then(res => JSON.parse(res.toString()));
   const [version] = release.name.match(/[\d|.]+/);
   if (version > pkg.version) {
     const notice = new Notification({
       title: 'Google 翻译',
       body: `发现新版本 (${release.name}) 可用，点击下载最新版本！`,
     });
-    notice.on('click', () =>
-      shell.openExternal(
-        'https://github.com/MoeFE/GoogleTranslate/releases/latest',
-      ),
-    );
+    notice.on('click', () => shell.openExternal(url));
     notice.show();
   }
 }
