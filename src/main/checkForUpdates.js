1f50b09df77f9fe750cb1d598e59584e34e70ca0
u3u <qwq@qwq.cat>
2018-08-29 10:25:30 +0800

    fix: unable to detect beta version update
    close #111

--- src/main/checkForUpdates.js
+++ src/main/checkForUpdates.js
@@ -5,8 +5,7 @@ import pkg from '../../package.json';
 export default async function checkForUpdates() {
   const url = 'https://api.github.com/repos/MoeFE/GoogleTranslate/releases/latest'; // prettier-ignore
   const release = await download(url).then(res => JSON.parse(res.toString()));
-  const [version] = release.name.match(/[\d|.]+/);
-  if (version > pkg.version) {
+  if (release.name > pkg.version) {
     const notice = new Notification({
       title: 'Google 翻译',
       body: `发现新版本 (${release.name}) 可用，点击下载最新版本！`,
